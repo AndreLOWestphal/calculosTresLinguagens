6fff35d2f4e00ef2243f2108dbeb7d0344842e19
AndreLOWestphal <dedewestphal@hotmail.com>
2024-03-28 13:25:41 -0300

    upando primeiro arquivo JS

---
+++ JavaScript/calculoBasico.js
@@ -0,0 +1,12 @@
+
+//Começaremos pedindo numeros e assim responderemos e os imprimiremos via alert, no browser.
+
+let numP1 = prompt('Digite um número: ');
+let numP2 = prompt('Digite um segundo número: ');
+
+
+// Estaremos concatenando de forma simples usando +
+console.log(alert("A soma dos números: " + (Number(numP1) + Number(numP2))))
+console.log(alert("A subtração dos números: " + (numP1 - numP2)))
+console.log(alert("A multiplicação dos números: " + (numP1 * numP2)))
+console.log(alert("A divisão dos números: " + (numP1 / numP2)))
