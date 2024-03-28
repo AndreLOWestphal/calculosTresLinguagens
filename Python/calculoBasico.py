e16e520d9b66878523b4503d8f31a6ddc5757d81
AndreLOWestphal <dedewestphal@hotmail.com>
2024-03-28 13:38:44 -0300

    adicinando codigo basico de calculo py

---
+++ Python/calculoBasico.py
@@ -0,0 +1,13 @@
+
+
+# Começaremos pedindo numeros e assim responderemos e os imprimiremos via alert, no browser.
+
+numP1 = int(input('Digite um número: '))
+numP2 = int(input('Digite um segundo número: '))
+
+
+# Estaremos concatenando de forma simples usando +
+print("A soma dos números: ", numP1 + numP2)
+print("A subtração dos números: ", numP1 - numP2)
+print("A multiplicação dos números: ", numP1 * numP2)
+print("A divisão dos números: ", numP1 / numP2)
