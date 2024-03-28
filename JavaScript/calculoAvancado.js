aedee7eba3eb58e0c1f7b89845ba19bdfaebcd1d
AndreLOWestphal <dedewestphal@hotmail.com>
2024-03-28 17:31:02 -0300

    Inseri o codigo no calcAvanc

--- JavaScript/calculoAvancado.js
+++ JavaScript/calculoAvancado.js
@@ -1,10 +1,47 @@
 
+let num1;
+let num2;
+let operador;
 
-let num1 = document.getElementById("campo1")
-let num2 = document.getElementById("campo2")
-let resultado = document.getElementById("resultado")
+num1 = prompt(`Digite um número para ser calculado: `);
+num2 = prompt(`Digite outro valor para ser calculado: `);
+operador = prompt(`Digite o operador de sua preferência, "+","-","*","/","**": `);
 
-num1.style.width = "80px";
-num1.style.marginLeft = "4px";
-num2.style.width = "80px";
-num2.style.marginLeft = "12px"
+console.log(alert(`O resultado do calculo é ${operar(num1, num2, operador)}!`))
+
+
+function operar(n1,n2,operacao){
+    if(operacao == "+"){
+        return soma(n1,n2)
+    } else if(operacao == "-"){
+        return subtracao(n1,n2)
+    } else if(operacao == "*"){
+        return multiplicacao(n1,n2)
+    } else if(operacao == "/"){
+        return divisao(n1,n2)
+    } else if(operacao == "**"){
+        return exponeciacao(n1,n2)
+    } else{
+        return "Erro interno operação não executada!"
+    }
+}
+
+function soma(x,y){
+    return x + y
+}
+
+function subtracao(x,y){
+    return x - y
+}
+
+function multiplicacao(x,y){
+    return x * y
+}
+
+function divisao(x,y){
+    return x / y
+}
+
+function exponeciacao(x,y){
+    return x**y
+}
