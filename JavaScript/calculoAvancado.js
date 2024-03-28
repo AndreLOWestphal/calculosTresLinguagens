e6b86f2a2c633bf99bf367a56224dcb4e68bd611
AndreLOWestphal <dedewestphal@hotmail.com>
2024-03-28 16:40:26 -0300

    upando JS avançado

---
+++ JavaScript/calculoAvancado.js
@@ -0,0 +1,10 @@
+
+
+let num1 = document.getElementById("campo1")
+let num2 = document.getElementById("campo2")
+let resultado = document.getElementById("resultado")
+
+num1.style.width = "80px";
+num1.style.marginLeft = "4px";
+num2.style.width = "80px";
+num2.style.marginLeft = "12px"
